ff29dc59c18b191fdffb96d5e9903c059c0b09a8
Amedeo Majer <amedeo@hive.fi>
2024-11-05 16:19:35 +0200

    feat: converted NavItem component to typescript

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { HTMLAttributes } from 'react';
 
-const NavItem = ({children, ...props}) => {
+interface NavItemProps extends HTMLAttributes<HTMLLIElement> {
+  children: string;
+}
+
+const NavItem:React.FC<NavItemProps> = ({children, ...props}) => {
   return (
     <li {...props} className='hover:lg:-translate-x-5 hover:cursor-pointer hover:opacity-75 transition-all duration-300 ease-in-out'>
       {children}
