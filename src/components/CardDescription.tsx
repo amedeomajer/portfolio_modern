e6b980101b0fbfb81a58cd0fba4e8915689df352
Amedeo Majer <amedeo@hive.fi>
2024-11-05 15:09:31 +0200

    feat: init typescript

---
+++ src/components/CardDescription.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+interface CardDescriptionProps {
+  children: ReactNode;
+}
+
+const CardDescription: React.FC<CardDescriptionProps> = ({ children }) => {
+  return (
+    <div className='py-2 px-4 text-justify text-xl rounded-sm'>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+export default CardDescription;
